e66d55cf76b1b5242fcf1159f8aafdc3647ceaad
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-03 13:24:25 +0530

    add all functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  return <div className="App">counter app</div>;
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({
+      type: "INC",
+    });
+  };
+  const decrement = () => {
+    dispatch({
+      type: "DEC",
+    });
+  };
+  const AddBy = () => {
+    dispatch({
+      type: "ADD",
+      payload: 10,
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={AddBy}>Add By 10</button>
+    </div>
+  );
 }
 
 export default App;
